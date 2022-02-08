f27f69aeaec2d1e4bfb0fa1cd12dc5792f6d1086
Solly Watkins <solly@sollyw.com>
2022-02-08 17:12:20 +1100

    Added FoodEffectModifier, idk if it works I can't be bothered testing it

---
+++ src/main/java/net/immortaldevs/sar/base/FoodStatusEffectModifier.java
@@ -0,0 +1,30 @@
+package net.immortaldevs.sar.base;
+
+import com.mojang.datafixers.util.Pair;
+import net.immortaldevs.sar.api.Modifier;
+import net.immortaldevs.sar.api.ModifierMap;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
+
+import java.util.List;
+
+@FunctionalInterface
+public interface FoodStatusEffectModifier extends Modifier {
+    void applyFoodStatusEffectModifier(
+            ItemStack stack,
+            World world,
+            LivingEntity targetEntity,
+            List<Pair<StatusEffectInstance, Float>> effects
+    );
+
+    @Override
+    default void register(ModifierMap modifierMap) {
+        modifierMap.merge(FoodStatusEffectModifier.class, this, (a, b) ->
+                (stack, world, targetEntity, effects) -> {
+                    a.applyFoodStatusEffectModifier(stack, world, targetEntity, effects);
+                    b.applyFoodStatusEffectModifier(stack, world, targetEntity, effects);
+                });
+    }
+}
--- src/main/java/net/immortaldevs/sar/mixin/LivingEntityMixin.java
+++ src/main/java/net/immortaldevs/sar/mixin/LivingEntityMixin.java
@@ -1,23 +1,49 @@
 package net.immortaldevs.sar.mixin;
 
+import com.mojang.datafixers.util.Pair;
+import net.immortaldevs.sar.api.RootComponentData;
 import net.immortaldevs.sar.base.FoodEffectModifier;
-import net.immortaldevs.sar.base.Util;
+import net.immortaldevs.sar.base.FoodStatusEffectModifier;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.item.ItemStack;
 import net.minecraft.world.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 @Mixin(LivingEntity.class)
 public abstract class LivingEntityMixin {
-    @Inject(method = "applyFoodEffects",
+    @ModifyVariable(method = "applyFoodEffects",
             at = @At(value = "INVOKE",
-                    target = "Lnet/minecraft/item/FoodComponent;getStatusEffects()Ljava/util/List;"))
-    private void applyFoodEffects(ItemStack stack, World world, LivingEntity targetEntity, CallbackInfo ci) {
-        FoodEffectModifier modifier = Util.getModifier(stack, FoodEffectModifier.class);
-        if (modifier == null) return;
-        modifier.applyFoodEffectModifier(stack, world, targetEntity);
+                    target = "Lnet/minecraft/item/FoodComponent;getStatusEffects()Ljava/util/List;",
+                    shift = At.Shift.BY,
+                    by = 2),
+            index = 5)
+    private List<Pair<StatusEffectInstance, Float>> applyFoodEffects(List<Pair<StatusEffectInstance, Float>> list, ItemStack stack, World world, LivingEntity targetEntity) {
+        Optional<RootComponentData> data = stack.sar$getComponentRoot();
+        if (data.isEmpty()) return list;
+
+        FoodEffectModifier foodEffectModifier = data.get()
+                .modifiers()
+                .get(FoodEffectModifier.class);
+
+        if (foodEffectModifier != null) {
+            foodEffectModifier.applyFoodEffectModifier(stack, world, targetEntity);
+        }
+
+        FoodStatusEffectModifier foodStatusEffectModifier = data.get()
+                .modifiers()
+                .get(FoodStatusEffectModifier.class);
+
+        if (foodStatusEffectModifier == null) return list;
+
+        List<Pair<StatusEffectInstance, Float>> effects = new ArrayList<>(list);
+        foodStatusEffectModifier.applyFoodStatusEffectModifier(stack, world, targetEntity, effects);
+        return effects;
     }
 }
